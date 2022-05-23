b182fb64c64f00c5886312b5a12342768582f9ce
AkshayPandey007 <101665005+AkshayPandey007@users.noreply.github.com>
2022-05-23 16:46:59 +0530

    first commit

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import Task from "../Task/Task";
+
+
 import styles from "./addTask.module.css";
 
 const AddTask = () => {
   // NOTE: do not delete `data-cy` key value pair
+  const[value,setValue] = useState("")
+  const[todos,setTodo]=useState([])
+
+  const onDelete=(id)=>{
+    let newTodo=todos.filter(todo=>todo.id!==id)
+    setTodo(newTodo)
+  }
   return (
     <div className={styles.todoForm}>
-      <input data-cy="add-task-input" type="text" />
-      <button data-cy="add-task-button"></button>
+      <input data-cy="add-task-input" type="text" placeholder="Add task..." className={styles.add}
+      value={value} onChange={(e)=>setValue(e.target.value)}/>
+      <button data-cy="add-task-button" onClick={()=>{
+       setTodo([...todos , {id:Date.now() , value:value}])
+       setValue("")
+      }}>Add</button>
+
+      {
+        todos.map((todo)=>(
+      <Task todo={todo} key={todo.id} onDelete={onDelete}/>
+        ))
+      }
+      
     </div>
   );
 };
--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./task.module.css";
 
-const Task = () => {
+const Task = ({todo,onDelete}) => {
   // NOTE: do not delete `data-cy` key value pair
+ const[isCompleted,setIsCompleted]=useState(todo.isCompleted)
+ const[count,setCount]=useState(0)
+
+ const increment=()=>{
+   setCount(count+1)
+ }
+
+ const decrement=()=>{
+   if(count>0){
+  setCount(count-1)
+   }
+}
   return (
     <li data-cy="task" className={styles.task}>
-      <input type="checkbox" data-cy="task-checkbox" />
-      <div data-cy="task-text"></div>
+      <input type="checkbox" data-cy="task-checkbox" checked={isCompleted} onChange={(e)=>{
+  setIsCompleted(e.target.checked)
+}}/>
+      <div data-cy="task-text" className={isCompleted ? styles.strike:""}>{todo.value}</div>
+      <button data-cy="task-remove-button"  onClick={()=>
+{onDelete(todo.id)}}>Delete </button>
       {/* Counter here */}
-      <button data-cy="task-remove-button"></button>
+   
+<button onClick={increment}>Inc</button>
+<h1>{count}</h1>
+<button onClick={decrement}>Dec</button>
+ 
     </li>
   );
 };
